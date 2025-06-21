8af9b59a506fd078d29b4aae27d12d8c1859a539
bdimon <regsdimon-reg@yahoo.com>
2025-06-21 19:19:37 +0200

    hide menu after choice item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { WpService } from './services/wp.service';
-import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonRouterOutlet, MenuController } from '@ionic/angular/standalone'; // Добавляем MenuController
 import { NgFor } from '@angular/common';
 
 @Component({
@@ -18,13 +18,14 @@ import { NgFor } from '@angular/common';
     IonList,
     IonItem,
     IonRouterOutlet,
+    // IonMenuToggle, // Убедимся, что IonMenuToggle здесь не импортируется, чтобы избежать предыдущей ошибки
     NgFor,
   ]
 })
 export class AppComponent implements OnInit {
   categories: any[] = [];
 
-  constructor(public api: WpService, private router: Router) {}
+  constructor(public api: WpService, private router: Router, private menuCtrl: MenuController) {} // Инжектируем MenuController
 
   ngOnInit() {
     this.api.loadCategories().subscribe(cats => {
@@ -33,7 +34,8 @@ export class AppComponent implements OnInit {
   }
 
   openCategory(cat_id: number) {
-    // Закрываем меню и переходим на страницу home с параметром cat_id
     this.router.navigate(['/home'], { queryParams: { cat_id } });
+    // Программно закрываем меню после навигации
+    this.menuCtrl.close();
   }
 }
